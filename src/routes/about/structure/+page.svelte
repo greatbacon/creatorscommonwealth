b7d76e4ea4edef644f11c6c12174723926de9e6c
greatbacon <hamiltonjreed@gmail.com>
2024-12-15 16:06:58 -0700

    Add struct & contact page

---
+++ src/routes/about/structure/+page.svelte
@@ -0,0 +1,28 @@
+<script>	
+</script>
+
+<div class="content">
+    <h1>Temporary Benevolent Dictatorship</h1>
+    <div class="text">
+        <p>
+            As Creators Commonwealth is still in its very early stages, i.e. consists of only 1 person at the moment, the project is technically being run as
+            a temporary benvolent dictatorship.
+        </p>
+
+        <p>
+            Upon the project reaching 5 founding members or the first dollar being processed via the platform, a new & more democratic structure will be adopted.            
+        </p>
+        <p>
+            For a possible vision of what the structure might look like, we will be keeping an eye on how
+            <a href="https://summereternal.com/#aboutus">Summer Eternals' structure</a> works out.
+        </p>
+    </div>	
+</div>
+
+<style>
+
+    .content {        
+        font-size: 2rem;        
+    }
+
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    const source = "YWRtaW5AY3JlYXRvcnNjb21tb253ZWFsdGgub3Jn";
+    const target = atob(source);
+</script>
+<h1>Get in Touch</h1>
+<div class="content">
+    <div class="text">
+        <p>
+            📧 {target}
+        </p>
+    </div>	
+</div>
+
+<style>
+    .content {        
+        font-size: 2rem;
+        text-align: center;        
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+</style>
