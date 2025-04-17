9b8528d0e6d4c5ff2aed68b3f8ada7a8eea0b452
greatbacon <hamiltonjreed@gmail.com>
2025-04-16 18:54:34 -0600

    Styling tweaks, spell check, and page formating.

--- src/routes/games/[game]/gameData.ts
+++ src/routes/games/[game]/gameData.ts
@@ -1,5 +1,5 @@
 import type {Image} from '$lib/imageGallery';
-import aqui1 from '$lib/images/aquisition/aquisition1.jpg';
+import aqui1 from '$lib/images/acquisition/acquisition1.jpg';
 
 export type Game = {
     name: string;
@@ -13,47 +13,59 @@ export type Game = {
 
 export type PriceSheet = {
     purchaseLink: string;
-    priceInCents: number;
+    priceDollar: number;
+    priceCents: number;
+    payWhatYouWant: boolean;
     productName: string;
     productDescription: string;
     creatorTitle: string;
     creatorVerb: string;
-    creatorName: string;    
+    creatorName: string;
+    outOfStock: boolean;    
 }
 
 export const games: Game[] = [{
-    name: "aquisition",
+    name: "acquisition",
     players: "2-6",
     age: "8+",
     playtime: "1hr",
     images: [{
         url: aqui1,
-        description: "A wooden box with the name Aquisition engraved on its lid, open to reveal game components"
+        description: "A wooden box with the name Acquisition engraved on its lid, open to reveal game components"
     }],
-    description: "Aquisition is a game about building an empire out of small companies. Each player takes turn placing tiles in an effort to found, expand, and merge various companies all while buying up stocks and making some money in the aquisition process. The player with the most money at the end wins. An average game takes about 1 hour.",
+    description: "Acquisition is a game about building an empire out of small companies. Each player takes turn placing tiles in an effort to found, expand, and merge various companies all while buying up stocks and making some money in the acquisition process. The player with the most money at the end wins. An average game takes about 1 hour.",
     priceSheet: [{
         purchaseLink: 'email admin',
-        priceInCents: 500,
+        priceDollar: 5,
+        priceCents: 0,
         productName: 'pattern',
         productDescription: 'Designed in-house by Creators Commonwealth, the pattern consists of finished Lightburn files for cutting out a board+tiles as well as a box. It also includes print ready PDFs for money, stock cards, cheat sheets, and an instruction 1 pager, plus the Nandeck script used to generate the money & stock cards.',
         creatorName: 'Creators Commonwealth',
         creatorTitle: 'designer',
         creatorVerb: 'designed',
+        payWhatYouWant: true,
+        outOfStock: false,
     },{
         purchaseLink: 'email admin',
-        priceInCents: 2500,
+        priceDollar: 25,
+        priceCents: 0,
         productName: 'kit',
         productDescription: 'Kits are fabricated in-house by Creators Commonwealth. They include 5 perforated, 12in x 12in x 1/8in birch plywood boards that can be assembled into the board and box, as well as all paper printed materials. Stocks, money, and cheat sheets will still need to be cut with scissors as well. Glue for assembly not included.',
         creatorName: 'Creators Commonwealth',
         creatorTitle: 'fabricator',
-        creatorVerb: 'fabricated'
+        creatorVerb: 'fabricated',
+        payWhatYouWant: false,
+        outOfStock: false,
     }, {
         purchaseLink: 'email admin',
-        priceInCents: 10000,
+        priceDollar: 100,
+        priceCents: 0,
         productName: 'full game',
         productDescription: 'A fully assembled copy ready for gifting or gaming. Finished wood, assembled board & box, pre-cut cards, plus a bag for tiles & 6 tile holders',
         creatorName: 'Creators Commonwealth',
         creatorTitle: 'assembler',
-        creatorVerb: 'assembled'
+        creatorVerb: 'assembled',
+        payWhatYouWant: false,
+        outOfStock: true,
     }]
 }];
